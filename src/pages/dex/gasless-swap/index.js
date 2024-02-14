51c95df00f4f82ec40856e3b6928d19bec13c5a7
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-14 17:57:09 +0530

    Update index.js

--- src/pages/dex/gasless-swap/index.js
+++ src/pages/dex/gasless-swap/index.js
@@ -7,7 +7,7 @@ export default function GaslessSwapPage() {
     <SwapLayout>
 
       <Wrapper>
-        <SwapBox title={"Gasless Swap"} />
+        <SwapBox title={"Lock Swap"} />
         <div className="relative p-5 text-[#D8D8D8] font-redHat font-medium space-y-4 text-base">
           <div className="absolute left-[-2rem] -top-[7.5rem] blur-[91.5px] bg-[#11FF37] opacity-25 h-80 w-80" />
           <div className=" hidden md:flex z-0 absolute right-[0rem] bottom-[-12rem] blur-[80px] bg-[#11FF37] opacity-25 h-60 w-60" />
